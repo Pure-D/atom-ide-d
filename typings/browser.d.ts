901dcf3b72de67b7fb04cc7dc1efcff4a699f5b8
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2021-01-27 01:36:38 -0600

    chore: remove typings folder

--- typings/browser.d.ts
+++
@@ -1,3 +0,0 @@
-/// <reference path="browser/ambient/atom/index.d.ts" />
-/// <reference path="browser/ambient/node/index.d.ts" />
-/// <reference path="browser/ambient/typescript-deferred/index.d.ts" />
--- typings/main.d.ts
+++
@@ -1,4 +0,0 @@
-/// <reference path="atomnew.d.ts" />
-/// <reference path="main/ambient/atom/index.d.ts" />
-/// <reference path="main/ambient/node/index.d.ts" />
-/// <reference path="main/ambient/typescript-deferred/index.d.ts" />
