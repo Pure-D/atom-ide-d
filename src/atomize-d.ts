0562112ac78aaecca0555ae56abe6de002e7f4a9
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2021-02-02 20:57:18 -0600

    fix: add some error handling

--- src/atomize-d.ts
+++ src/atomize-d.ts
@@ -21,10 +21,25 @@ class DLanguageClient extends AutoLanguageClient {
     return "serve-d"
   }
 
-  startServerProcess(projectPath: string) {
+  async startServerProcess(projectPath: string) {
     const serveD = spawn(serverByOS[process.platform], [], {
       cwd: projectPath,
     })
+
+    serveD.on("error", (err) => {
+      atom.notifications.addError(err.message, { description: err.name, stack: err.stack })
+    })
+
+    serveD.on("close", (code, signal) => {
+      if (code !== 0 && signal == null) {
+        atom.notifications.addError("Unable to start the Serve-D language server.\nCheck the dev tools console for more information.")
+      }
+    })
+
+    serveD.on("exit", () => {
+      atom.notifications.addError("Serve-D crashed.\nCheck the dev tools console for more information.")
+    })
+
     return serveD
   }
 }
