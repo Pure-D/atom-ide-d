23dda4e153438aff1929f06493a5aed8a94dc740
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2021-02-24 16:23:38 -0600

    fix: only download the asset for that platform

--- src/get-serve-d.ts
+++ src/get-serve-d.ts
@@ -1,32 +1,39 @@
 import downloadRelease from "@terascope/fetch-github-release"
-import { join, dirname, basename, extname } from "path"
+import { join, dirname, extname } from "path"
 import { remove, ensureDir } from "fs-extra"
 import decompress from "decompress"
 import decompressTarxz from "decompress-tarxz"
+
+const assetMap = {
+  win32: "windows",
+  darwin: "osx",
+  linux: "linux",
+}
 
 // function to download serve-d binaries from GitHub
 export async function getServeD() {
   const distFolder = join(dirname(__dirname), "dist")
   await remove(distFolder)
   await ensureDir(distFolder)
+
+  const platform = assetMap[process.platform]
+
   const assets = ((await downloadRelease(
-    "Pure-D",
-    "serve-d",
-    distFolder,
-    undefined,
-    undefined,
+    /* username */ "Pure-D",
+    /* repo */ "serve-d",
+    /* download folder */ distFolder,
+    /* filter asset */ undefined,
+    /* filter asset */ (asset) => asset.name.indexOf(platform) >= 0,
     true
   )) as unknown) as string[]
-  for (const asset of assets) {
-    const platform = basename(asset).match(/windows|linux|osx/)[0]
-    extname(asset)
-    if (extname(asset) === ".xz") {
-      await decompress(asset, join(distFolder, platform), {
-        plugins: [decompressTarxz()],
-      })
-    } else {
-      await decompress(asset, join(distFolder, platform))
-    }
-    remove(asset)
+
+  const asset = assets[0]  // Assume there is only one possibility
+  if (extname(asset) === ".xz") {
+    await decompress(asset, join(distFolder, platform), {
+      plugins: [decompressTarxz()],
+    })
+  } else {
+    await decompress(asset, join(distFolder, platform))
   }
+  remove(asset)
 }
