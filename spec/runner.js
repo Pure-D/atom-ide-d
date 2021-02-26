0b656ee436ae2e8a9215cd68b22bf29883c081a3
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2021-02-25 19:56:49 -0600

    chore: set specHepler to true [skip ci]

--- spec/runner.js
+++ spec/runner.js
@@ -6,21 +6,5 @@ import pkg from "../package.json"
 export default createRunner({
   testPackages: Array.from(pkg["package-deps"]),
   timeReporter: true,
-  // Extra Packages
-  specHelper: {
-    atom: true,
-    attachToDom: true,
-    ci: true,
-    customMatchers: true,
-    jasmineFocused: false,
-    jasmineJson: false,
-    jasminePass: false,
-    jasmineShouldFail: false,
-    jasmineTagged: false,
-    mockClock: true,
-    mockLocalStorage: false,
-    profile: true,
-    set: false,
-    unspy: false,
-  },
+  specHelper: true
 })
