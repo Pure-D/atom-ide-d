95b852dabd7bbbfa6c25b91c7b06c7d090326cff
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2021-02-27 14:46:38 -0600

    feat: automatically install atom-ide-base

--- src/ide-d.ts
+++ src/ide-d.ts
@@ -2,6 +2,17 @@ import { AutoLanguageClient } from "atom-languageclient"
 import { installServeD } from "./installation"
 
 class DLanguageClient extends AutoLanguageClient {
+  activate() {
+    super.activate()
+    if (!atom.packages.isPackageLoaded("atom-ide-base")) {
+      // install if not installed
+      // eslint-disable-next-line @typescript-eslint/no-var-requires
+      require("atom-package-deps").install("ide-d", true)
+      // enable if disabled
+      atom.packages.enablePackage("atom-ide-base")
+    }
+  }
+
   getGrammarScopes() {
     return ["source.d", "D"]
   }
